e77504fedc9bcddf6049058ec9cbc74e48b43bfe
Vadim <sav626@mail.com>
2017-01-19 12:07:06 +0300

    Update model

--- main.rb
+++ main.rb
@@ -12,8 +12,8 @@ array = Array.new
 array[1] = 1
 array[0] = "try_this"
 
-items_to_update = {:name => "name", :old_value => "'try_this'", :new_value => "'new_try'"}
-items_to_del = {:name => "name", :value => "'new_try'"}
+items_to_update = {:name => "name", :old_value => "try_this", :new_value => "new_try"}
+items_to_del = {:name => "name", :value => "new_try"}
 
 #orm.add_items("block", array)
 #orm.delete_items("block", items_to_del)
@@ -21,15 +21,22 @@ items_to_del = {:name => "name", :value => "'new_try'"}
 #orm.get_items("block")
 
 
-block = Block.new(options)
+#block = Block.new(options)
 #block.create
 #block.get_item
-block.add_item(array)
-block.get_item
-block.update_item(items_to_update)
-block.get_item
-block.delete_item(items_to_del)
-block.get_item
-
+#block.add_item(array)
+#block.get_item
+#block.update_item(items_to_update)
+#block.get_item
+#block.delete_item(items_to_del)
+#block.get_item
+Block.conn(options)
+Block.add_items(array)
+Block.get_items
+Block.update_items(items_to_update)
+Block.get_items
+Block.delete_items(items_to_del)
+Block.get_items
+Block.close_connection
 
 
--- orm.rb
+++ orm.rb
@@ -1,94 +1,102 @@
 class Orm
 
-  def initialize(options)
-    options[:host] = "127.0.0.1"
-    @con = PGconn.new(options)
-  end
-
  # def create_sequence
    # @con.exec("CREATE SEQUENCE \"slack\";")
  # end
 
-  private
+  class << self
 
-  def create_table (table_name)
-     query = "CREATE TABLE #{table_name} (id INTEGER PRIMARY KEY, name VARCHAR(64));"
-    @con.exec(query)
-  end
-
-  def add_items(table_name, items)
-    prepare_insert(table_name)
-    @con.exec_prepared("insert_#{table_name}", [items[0],items[1]])
-  end
-
-  def get_items(table_name)
-    items = Array.new
-    @con.exec("SELECT * FROM #{table_name}") do |result|
-      i = result.count - 1
-      while (i >= 0)
-       items << result[i]
-        i -= 1
-      end
+    def conn(options)
+      options[:host] = "127.0.0.1"
+      @con = PGconn.new(options)
+      @table_name = class_name
     end
-    p items
-  end
 
-  def update_items(table_name, items)
-    @con.exec("UPDATE #{table_name} SET #{items[:name]} = #{items[:new_value]}
-               WHERE #{items[:name]} = #{items[:old_value]};")
-  end
+    attr_reader :table_name
 
-  def delete_items(table_name, element)
-    @con.exec("DELETE FROM #{table_name} WHERE #{element[:name]} = #{element[:value]};")
-  end
-
-  def delete_table(table_name)
-    @con.exec("DROP TABLE IF EXISTS #{table_name}")
-  end
-
-  def close_connection
-    @con.close
-  end
-
-  def prepare_insert(table_name)
-    fields = get_table_fields(table_name)
-    str = ""
-    i =1
-    while i <= fields[1]
-      unless i == fields[1]
-        str+= "($#{i}, "
-        i += 1
-      else
-        str += "$#{i})"
-        i += 1
-      end
+    def create_table
+      query = "CREATE TABLE #{@table_name} (id INTEGER PRIMARY KEY, name VARCHAR(64));"
+      @con.exec(query)
     end
-    @con.prepare("insert_#{table_name}", "insert into #{table_name} (#{fields[0]}) values #{str}")
-  end
 
-  def get_table_fields(table_name)
-    fields = Array.new
-    str = ""
-    count = 0
-    @con.exec("SELECT column_name
-               FROM INFORMATION_SCHEMA.COLUMNS
-               WHERE table_name = '#{table_name}';") do |result|
-      i = result.count - 1
-      count = i+1
-      while (i >= 0)
-        if (i > 0)
-          str += result[i]["column_name"] + ", "
+    def add_items(items)
+      prepare_insert
+      @con.exec_prepared("insert_#{@table_name}", [items[0],items[1]])
+    end
+
+    def get_items
+      items = Array.new
+      @con.exec("SELECT * FROM #{@table_name}") do |result|
+        i = result.count - 1
+        while (i >= 0)
+         items << result[i]
           i -= 1
-
-        else
-          str += result[i]["column_name"]
-          i -=1
         end
       end
+      p items
     end
-    fields[0] = str
-    fields[1] = count
-    return fields
-  end
 
+    def update_items(items)
+      @con.exec("UPDATE #{@table_name} SET #{items[:name]} = '#{items[:new_value]}'
+                 WHERE #{items[:name]} = '#{items[:old_value]}';")
+    end
+
+    def delete_items(element)
+      @con.exec("DELETE FROM #{@table_name} WHERE #{element[:name]} = '#{element[:value]}';")
+    end
+
+    def delete_table
+      @con.exec("DROP TABLE IF EXISTS #{@table_name}")
+    end
+
+    def close_connection
+      @con.close
+    end
+
+    def prepare_insert
+      fields = get_table_fields
+      str = ""
+      i =1
+      while i <= fields[1]
+        unless i == fields[1]
+          str+= "($#{i}, "
+          i += 1
+        else
+          str += "$#{i})"
+          i += 1
+        end
+      end
+      @con.prepare("insert_#{@table_name}", "insert into #{@table_name} (#{fields[0]}) values #{str}")
+    end
+
+    def get_table_fields
+      fields = Array.new
+      str = ""
+      count = 0
+      @con.exec("SELECT column_name
+                 FROM INFORMATION_SCHEMA.COLUMNS
+                 WHERE table_name = '#{@table_name}';") do |result|
+        i = result.count - 1
+        count = i+1
+        while (i >= 0)
+          if (i > 0)
+            str += result[i]["column_name"] + ", "
+            i -= 1
+
+          else
+            str += result[i]["column_name"]
+            i -=1
+          end
+        end
+      end
+      fields[0] = str
+      fields[1] = count
+      return fields
+    end
+
+    def class_name
+      self.name.to_s.downcase + "s"
+    end
+
+  end
 end
